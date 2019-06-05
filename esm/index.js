58691d7f3b2e0282952d235132ae3db8e9af59b0
hewIngram <hew@gousto.co.uk>
2019-06-05 08:22:52 +0100

    add built files

--- esm/index.js
+++ esm/index.js
@@ -1,43 +1,61 @@
 /* eslint-disable no-console */
 
 const chalk = require("chalk");
-const colorize = require("json-colorizer");
-const sqlFormatter = require('sql-formatter');
+const sqlFormatter = require("sql-formatter");
+const utils = require('./utils');
 
-const JSONifier = string => {
-    if( typeof string === 'object' ) {
-        return colorize(JSON.stringify(string))
+exports.error = (...inputs) => {
+    if (utils.checkForSpecifiError(inputs)) {
+        utils.printer("error", utils.constuctError(inputs[0], inputs[1]));
+    } else {
+        inputs.forEach(arg => {
+            utils.printer("error", arg);
+        });
     }
-    try {
-        JSON.parse(string);
-    } catch (e) {
-        return string
-    }
-    return colorize(string)
 };
 
-exports.error = (...theArgs) => {
-    theArgs.forEach(arg => {
-        console.log(chalk.red(`🤯   [ERROR]  ${JSONifier(arg)}`));
+exports.warn = (...inputs) => {
+    if (utils.checkForSpecifiError(inputs)) {
+        utils.printer("warn", utils.constuctError(inputs[0], inputs[1]));
+    } else {
+        inputs.forEach(arg => {
+            utils.printer("warn", arg);
+        });
+    }
+};
+
+exports.info = (...inputs) => {
+    if (utils.checkForSpecifiError(inputs)) {
+        utils.printer("info", utils.constuctError(inputs[0], inputs[1]));
+    } else {
+        inputs.forEach(arg => {
+            utils.printer("info", arg);
+        });
+    }
+};
+
+exports.log = (...inputs) => {
+    if (utils.checkForSpecifiError(inputs)) {
+        utils.printer("log", utils.constuctError(inputs[0], inputs[1]));
+    } else {
+        inputs.forEach(arg => {
+            utils.printer("log", arg);
+        });
+    }
+};
+
+exports.debug = (...inputs) => {
+    if (utils.checkForSpecifiError(inputs)) {
+        utils.printer("debug", utils.constuctError(inputs[0], inputs[1]));
+    } else {
+        inputs.forEach(arg => {
+            utils.printer("debug", arg);
+        });
+    }
+};
+
+exports.sql = (...inputs) => {
+    inputs.forEach(arg => {
+        console.log(chalk.grey(`[SQLIZE] ${sqlFormatter.format(arg)}`));
     });
-};
-
-exports.warn = data => {
-    console.log(chalk.yellow(`⚠️   [WARN]   ${JSONifier(data)}`));
-};
-
-exports.info = data => {
-    console.log(chalk.green(`🧙‍   [INFO]   ${JSONifier(data)}`));
-};
-
-exports.log = data => {
-    console.log(`🌳  [LOG]    ${JSONifier(data)}`);
-};
-
-exports.debug = data => {
-    console.log(chalk.blue(`🕵️‍   [DEBUG]  ${JSONifier(data)}`));
-};
-
-exports.sql = data => {
-    console.log(chalk.grey(`[SQLIZE] ${sqlFormatter.format(data)}`));
 };
